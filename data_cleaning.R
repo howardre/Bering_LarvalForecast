0fe155acc483d50417c379e05fece9b65f7dac4c
Rebecca Howard <howardrebecca@gmail.com>
2021-05-24 13:32:10 -0700

    Add functions to trim, clean, finalize data

--- data_cleaning.R
+++ data_cleaning.R
@@ -186,8 +186,39 @@ data_check <- function(egg, larvae){
        xlab = 'Lat',
        main = 'Larvae')
 }
-clean_data <- function(){
-  
+clean_data <- function(data_formatted){
+  # Most sampled net is 1, but there are instances of net 2 (and 3!) being sampled.
+  data_formatted[data_formatted$net == 3, ] # All net 3 samples are from a 153 um mesh size. They need to be discarded.
+  data_two <- data_formatted[data_formatted$net < 3, ] # Let us now check the net 2 samples
+  # There are 659 stations with net = 2 recorded
+  data_tmp <- table(data_two$id)
+  # Of these 84 have been sampled twice, which means that we only need to retain one of the two nets from these 84 stations
+  data_net2 <- names(data_tmp)[data_tmp == 2] # ID of stations with 2 net records
+  data_net1 <- data_formatted[data_formatted$net == 1, ] # These are data for records with net 1 only (n=5495)
+  data_both <- data_two[data_two$id %in% id_2 &
+                                data_two$net == 1, ] # These are the data in which two nets were recorded, but I am only retaining net 1. We now need to remove data that were recorded with 2 nets, and then re-attach data with only net 1 (n=84)
+  data_one <- data_two[!data_two$id %in% id_2, ] # These are data where only 1 net was recorded, either 1 or 2 (n=5495+660-84*2=5987)
+  data_clean <- rbind(data_one, data_both)
+  data_clean <- data_clean[data_clean$primary_net == 'Y', ]
+  return(data_clean)
+}
+trim_data <- function(data_clean){
+  data_clean <- data_clean[data_clean$year > 1987, ]
+  # For each station, add distance to closest positive catch
+  data_clean$dist <- NA
+  for (i in 1:nrow(data_clean)) {
+    data_clean$dist[i] <- min(distance_function(data_clean$lat[i], 
+                                             data_clean$lon[i], 
+                                             data_clean$lat[data_clean$larvalcatchper10m2 > 0], 
+                                             data_clean$lon[data_clean$larvalcatchper10m2 > 0]))
+  }
+  data_final <- return(mutate(data_clean, date = make_date(year, month, day)))
+}
+final_data <- function(data_trim){
+  data_subset <- data_trim[data_trim$dist < 30000, 
+                            c('larvalcatchper10m2', 'year', 'lat', 'lon', 'doy', 'date')]
+  names(data_subset) <- c('larvalcatchper10m2', 'year', 'lat', 'lon', 'doy', 'date')
+  return(data_subset)
 }
 
 ### Yellowfin Sole ----
@@ -196,146 +227,91 @@ yfs_egg_formatted <- format_data(yfs_egg_raw)
 yfs_larvae_formatted <- format_data(yfs_larvae_raw)
 
 # Check attributes of egg and larval data
-windows()
 data_check(yfs_egg_formatted, yfs_larvae_formatted)
 
 table(yfs_egg_formatted$haul_performance)
 table(yfs_larvae_formatted$haul_performance)
 
 # Egg data: check number of bongo nets per each station
-# These appear to be the same for all species, therefore use function
-table(yfs_egg_raw$net) # Most sampled net is 1, but there are instances of net 2 (and 3!) being sampled.
-yfs_egg_raw[yfs_egg_raw$net == 3, ] # All net 3 samples are from a 153 um mesh size. They need to be discarded.
-yfs_egg_1_2 <- yfs_egg_raw[yfs_egg_raw$net < 3, ] # Let us now check the net 2 samples
-table(yfs_egg_1_2$net) # There are 659 stations with net = 2 recorded
-tmp <- table(yfs_egg_1_2$id)
-table(tmp) # Of these 84 have been sampled twice, which means that we only need to retain one of the two nets from these 84 stations
-id_2 <- names(tmp)[tmp == 2] # ID of stations with 2 net records
-yfs_egg_1 <- yfs_egg_1_2[yfs_egg_1_2$net == 1, ] # These are data for records with net 1 only (n=5495)
-dim(yfs_egg_1)
-yfs_egg_2 <- yfs_egg_1_2[yfs_egg_1_2$id %in% id_2 &
-               yfs_egg_1_2$net == 1, ] # These are the data in which two nets were recorded, but I am only retaining net 1. We now need to remove data that were recorded with 2 nets, and then re-attach data with only net 1 (n=84)
-dim(yfs_egg_2)
-
-yfs_egg_1net <- yfs_egg_1_2[!yfs_egg_1_2$id %in% id_2, ] # These are data where only 1 net was recorded, either 1 or 2 (n=5495+660-84*2=5987)
-dim(yfs_egg_1net)
-yfs_egg <- rbind(yfs_egg_1net, yfs_egg_2)
-dim(yfs_egg) # Original n = 6182, of which n = 27 were net 3 (discarded), and n = 84*2 had two nets recorded. We should end up with n = 6181-27-84= 6070 data points
-
-# Larval catch data: check number of bongo nets per each station
-# These appear to be the same for all species, therefore use function
-table(yfs_larvae_raw$net) # Most sampled net is 1, but there are instances of net 2 (n=659) being sampled.
-yfs_larvae_raw[yfs_larvae_raw$net == 3, ] # All net 3 samples are from a 153 um mesh size. They need to be discarded.
-yfs_larvae_1_2 <- yfs_larvae_raw[yfs_larvae_raw$net < 3, ] # Let us now check the net 2 samples
-table(yfs_larvae_1_2$net) # There are 659 stations with net = 2 recorded
-tmp <- table(yfs_larvae_1_2$id)
-table(tmp) # Of these 84 have been sampled twice, which means that we only need to retain one of the two nets from these 84 stations
-id_2 <- names(tmp)[tmp == 2] #ID of stations with 2 net records
-yfs_larvae_2 <- yfs_larvae_1_2[yfs_larvae_1_2$id %in% id_2 &
-                  yfs_larvae_1_2$net == 1, ] # These are the data in which two nets were recorded, but I am only retaining net 1 (n = 84).
-dim(yfs_larvae_2)
-# We now need to remove data that were recorded with 2 nets (n=84), and then re-attach data with only net 1
-
-yfs_larvae_1net <- yfs_larvae_1_2[!yfs_larvae_1_2$id %in% id_2, ]#These are data where only 1 net was recorded, either 1 or 2 (n=net1+net2-net1&2=5495+660-84*2=5987)
-dim(yfs_larvae_1net)
-yfs_larvae <- rbind(yfs_larvae_1net, yfs_larvae_2)
-dim(yfs_larvae)#Original n = 6182, of which n = 27 were net 3 (discarded), and n = 84*2 had two nets recorded. We should end up with n = 6181-27-84= 6070 data points
+# These appear to be the same for all species, therefore use clean_data function for all other species
+# table(yfs_egg_formatted$net) 
+# yfs_egg_formatted[yfs_egg_formatted$net == 3, ] 
+# yfs_egg_two <- yfs_egg_formatted[yfs_egg_formatted$net < 3, ] 
+# yfs_egg_tmp <- table(yfs_egg_two$id)
+# yfs_egg_net2 <- names(yfs_egg_tmp)[yfs_egg_tmp == 2] 
+# yfs_egg_net1 <- yfs_egg_formatted[yfs_egg_formatted$net == 1, ] 
+# dim(yfs_egg_net1)
+# yfs_egg_both <- yfs_egg_two[yfs_egg_two$id %in% id_2 &
+#                yfs_egg_two$net == 1, ] 
+# dim(yfs_egg_both)
+# yfs_egg_one <- yfs_egg_two[!yfs_egg_two$id %in% id_2, ] 
+# dim(yfs_egg_one)
+# yfs_egg <- rbind(yfs_egg_one, yfs_egg_both)
+# dim(yfs_egg)
+# 
+# # Larval catch data: check number of bongo nets per each station
+# # These appear to be the same for all species, use clean_data
+# table(yfs_larvae_raw$net) 
+# yfs_larvae_raw[yfs_larvae_raw$net == 3, ] 
+# yfs_larvae_1_2 <- yfs_larvae_raw[yfs_larvae_raw$net < 3, ] 
+# table(yfs_larvae_1_2$net) 
+# tmp <- table(yfs_larvae_1_2$id)
+# table(tmp) 
+# id_2 <- names(tmp)[tmp == 2]
+# yfs_larvae_2 <- yfs_larvae_1_2[yfs_larvae_1_2$id %in% id_2 &
+#                   yfs_larvae_1_2$net == 1, ] 
+# dim(yfs_larvae_2)
+# yfs_larvae_1net <- yfs_larvae_1_2[!yfs_larvae_1_2$id %in% id_2, ]
+# dim(yfs_larvae_1net)
+# yfs_larvae <- rbind(yfs_larvae_1net, yfs_larvae_2)
+# dim(yfs_larvae)
 
 # As per Lauren email (9/12/19) only retain station where primary_net == Y
-table(yfs_egg$primary_net)
-table(yfs_larvae$primary_net)
-yfs_egg <- yfs_egg[yfs_egg$primary_net == 'Y', ]
-yfs_larvae <- yfs_larvae[yfs_larvae$primary_net == 'Y', ]
-table(yfs_egg$primary_net)
-table(yfs_larvae$primary_net)
+# table(yfs_egg$primary_net)
+# table(yfs_larvae$primary_net)
+# yfs_egg <- yfs_egg[yfs_egg$primary_net == 'Y', ]
+# yfs_larvae <- yfs_larvae[yfs_larvae$primary_net == 'Y', ]
 
-##### Trim egg and larval data -----
+# Clean up data to remove unnecessary hauls
+yfs_egg_clean <- clean_data(yfs_egg_formatted)
+yfs_larvae_clean <- clean_data(yfs_larvae_formatted)
+
+table(yfs_egg_clean$primary_net)
+table(yfs_larvae_clean$primary_net)
+
+##### Trim egg and larval data
 # Year: 1988 forward
 # Month: all
 # Latitude: all
-yfs_egg <- yfs_egg[yfs_egg$year > 1987, ]
-yfs_larvae <- yfs_larvae[yfs_larvae$year > 1987, ]
-
-# For each station, add distance to closest positive catch
-yfs_egg$dist <- NA
-yfs_larvae$dist <- NA
-for (i in 1:nrow(yfs_egg)) {
-  yfs_egg$dist[i] <- min(distance_function(yfs_egg$lat[i], 
-                                          yfs_egg$lon[i], 
-                                          yfs_egg$lat[yfs_egg$larvalcatchper10m2 > 0], 
-                                          yfs_egg$lon[yfs_egg$larvalcatchper10m2 > 0]))
-  yfs_larvae$dist[i] <- min(distance_function(yfs_larvae$lat[i],
-                                             yfs_larvae$lon[i],
-                                             yfs_larvae$lat[yfs_larvae$larvalcatchper10m2 > 0],
-                                             yfs_larvae$lon[yfs_larvae$larvalcatchper10m2 > 0]))
-}
-yfs_egg <- mutate(yfs_egg, date = make_date(year, month, day))
-yfs_larvae <- mutate(yfs_larvae, date = make_date(year, month, day))
+yfs_egg_trim <- trim_data(yfs_egg_clean)
+yfs_larvae_trim <- trim_data(yfs_larvae_clean)
 
 # Inspect new data
-windows()
-par(mfrow = c(2, 4))
-plot(table(yfs_egg$year[yfs_egg$larvalcatchper10m2 > 0]),
-     ylab = 'Frequency',
-     xlab = 'Year',
-     main = 'Eggs')
-plot(table(yfs_egg$month[yfs_egg$larvalcatchper10m2 > 0]),
-     ylab = 'Frequency',
-     xlab = 'Month',
-     main = 'Eggs')
-plot(table(yfs_egg$gear_name[yfs_egg$larvalcatchper10m2 > 0]),
-     ylab = 'Frequency',
-     xlab = 'Gear',
-     main = 'Eggs')
-hist(yfs_egg$lat[yfs_egg$larvalcatchper10m2 > 0],
-     ylab = 'Frequency',
-     xlab = 'Lat',
-     main = 'Eggs')
-plot(table(yfs_larvae$year[yfs_larvae$larvalcatchper10m2 > 0]),
-     ylab = 'Frequency',
-     xlab = 'Year',
-     main = 'Larvae')
-plot(table(yfs_larvae$month[yfs_larvae$larvalcatchper10m2 > 0]),
-     ylab = 'Frequency',
-     xlab = 'Month',
-     main = 'Larvae')
-plot(table(yfs_larvae$gear_name[yfs_larvae$larvalcatchper10m2 > 0]),
-     ylab = 'Frequency',
-     xlab = 'Gear',
-     main = 'Larvae')
-hist(yfs_larvae$lat[yfs_larvae$larvalcatchper10m2 > 0],
-     ylab = 'Frequency',
-     xlab = 'Lat',
-     main = 'Larvae')
+data_check(yfs_egg_trim, yfs_larvae_trim)
 
-
-# Select data for constraint analyses, including stations that are <30km away from the closest positive catch
-yfs_subset_egg <- yfs_egg[yfs_egg$dist < 30000, 
-                     c('larvalcatchper10m2', 'year', 'lat', 'lon', 'doy', 'date')]
-yfs_complete_larvae <- yfs_larvae[yfs_larvae$dist < 30000, 
-                           c('larvalcatchper10m2', 'year', 'lat', 'lon', 'doy', 'date')]
-names(yfs_subset_egg) <- c('larvalcatchper10m2', 'year', 'lat', 'lon', 'doy', 'date')
-names(yfs_complete_larvae) <- c('larvalcatchper10m2', 'year', 'lat', 'lon', 'doy', 'date')
+# Select data for constrained analyses, including stations that are <30km away from the closest positive catch
+yfs_subset_egg <- final_data(yfs_egg_trim)
+yfs_subset_larvae <- final_data(yfs_larvae_trim)
 table(yfs_subset_egg$year)
-table(yfs_complete_larvae$year)
+table(yfs_subset_larvae$year)
 
-#### Split egg and larvae data into separate datasets by date ----
+#### Split egg and larvae data into separate datasets by date
 yfs_subset_egg1 <- filter(yfs_subset_egg, year < 1990)
 yfs_complete_larvae1 <- filter(yfs_subset_egg, year < 1990)
 yfs_subset_egg2 <- filter(yfs_subset_egg, year > 1989 & year < 1995)
-yfs_complete_larvae2 <- filter(yfs_complete_larvae, year > 1989 & year < 1995)
+yfs_complete_larvae2 <- filter(yfs_subset_larvae, year > 1989 & year < 1995)
 yfs_subset_egg3 <- filter(yfs_subset_egg, year > 1994 & year < 2000)
-yfs_complete_larvae3 <- filter(yfs_complete_larvae, year > 1994 & year < 2000)
+yfs_complete_larvae3 <- filter(yfs_subset_larvae, year > 1994 & year < 2000)
 yfs_subset_egg4 <- filter(yfs_subset_egg, year > 1999 & year < 2005)
-yfs_complete_larvae4 <- filter(yfs_complete_larvae, year > 1999 & year < 2005)
+yfs_complete_larvae4 <- filter(yfs_subset_larvae, year > 1999 & year < 2005)
 yfs_subset_egg5 <- filter(yfs_subset_egg, year > 2004 & year < 2010)
-yfs_complete_larvae5 <- filter(yfs_complete_larvae, year > 2004 & year < 2010)
+yfs_complete_larvae5 <- filter(yfs_subset_larvae, year > 2004 & year < 2010)
 yfs_subset_egg6 <- filter(yfs_subset_egg, year > 2009 & year < 2015)
-yfs_complete_larvae6 <- filter(yfs_complete_larvae, year > 2009 & year < 2015)
+yfs_complete_larvae6 <- filter(yfs_subset_larvae, year > 2009 & year < 2015)
 yfs_subset_egg7 <- filter(yfs_subset_egg, year > 2014 & year < 2020)
-yfs_complete_larvae7 <- filter(yfs_complete_larvae, year > 2014 & year < 2020)
+yfs_complete_larvae7 <- filter(yfs_subset_larvae, year > 2014 & year < 2020)
 
-#### Add Bering10K model temperatures and salinities ----
+#### Add Bering10K model temperatures and salinities
 varid_match <- function(data, model_output1, model_output2){
   data$roms_date <- NA
   data$roms_temperature <- NA
