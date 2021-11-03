58f42d79424a772cbd5124adb7f5747cec19232f
Rebecca Howard <howardrebecca@gmail.com>
2021-11-03 16:17:07 -0700

    Change color, font size, line size

--- code/functions/plot_variable.R
+++ code/functions/plot_variable.R
@@ -3,15 +3,15 @@ plot_variable <- function(gam, covariate, bounds, variable, ylabel, yvalues){
        pages = 0,
        select = covariate, # 1 = year/PDO/NPGO, 2 = lat/lon, 3 = depth, 4 = julian, 5 = temp
        shade = T,
-       shade.col = "lemonchiffon3",
+       shade.col = "#eb8055b2",
        ylim = bounds,
        xlab = variable,
        ylab = ylabel,
        yaxt = yvalues,
        seWithMean = T,
        scale = 0,
-       cex.axis = 6,
-       cex.lab = 6,
+       cex.axis = 6.5,
+       cex.lab = 6.5,
        family = "serif",
-       lwd = 2)
+       lwd = 2.5)
 }
