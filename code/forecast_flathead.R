cd67c6b47d07553b534c0d724ac4fee606535017
Rebecca Howard <howardrebecca@gmail.com>
2022-01-12 09:38:53 -0800

    Reduce grid size, add function to averages to make figures with same scale

--- code/forecast_flathead.R
+++ code/forecast_flathead.R
@@ -50,8 +50,8 @@ varid_match <- function(data, model_output1, model_output2, list){
 
 # Function to make maps
 grid_predict <- function(grid, title){
-  nlat = 80
-  nlon = 120
+  nlat = 40
+  nlon = 60
   latd = seq(min(grid$lat), max(grid$lat), length.out = nlat)
   lond = seq(min(grid$lon), max(grid$lon), length.out = nlon)
   my_color = colorRampPalette(rev(c("#FFFFCC", "#FBF2A8", "#F9E585",
@@ -139,8 +139,8 @@ get_preds <- function(data, year, date, doy,
                       temp_output, salt_output,
                       list, formula){
   # Prediction grid
-  nlat = 80
-  nlon = 120
+  nlat = 40
+  nlon = 60
   latd = seq(min(data$lat), max(data$lat), length.out = nlat)
   lond = seq(min(data$lon), max(data$lon), length.out = nlon)
   grid_extent <- expand.grid(lond, latd)
@@ -2390,7 +2390,62 @@ dev.copy(jpeg,
 dev.off()
 
 ### Average Predictions ------------------------------------------------------------------------------------------------------------
-
+# Change to make same zlim
+grid_predict <- function(grid, title){
+  nlat = 40
+  nlon = 60
+  latd = seq(min(grid$lat), max(grid$lat), length.out = nlat)
+  lond = seq(min(grid$lon), max(grid$lon), length.out = nlon)
+  my_color = colorRampPalette(rev(c("#FFFFCC", "#FBF2A8", "#F9E585",
+                                    "#F5D363", "#EFBA55", "#EAA352",
+                                    "#E68C51", "#E0754F", "#D75C4D",
+                                    "#BB4A48", "#994240", "#763931", 
+                                    "#542D20", "#352311", "#191900")))
+  image(lond,
+        latd,
+        t(matrix(grid$avg_pred,
+                 nrow = length(latd),
+                 ncol = length(lond),
+                 byrow = T)),
+        xlim = c(-176.5, -156.5),
+        ylim = c(52, 62),
+        axes = FALSE,
+        xlab = "",
+        ylab = "")
+  rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "mintcream")
+  par(new = TRUE)
+  image(lond,
+        latd,
+        t(matrix(grid$avg_pred,
+                 nrow = length(latd),
+                 ncol = length(lond),
+                 byrow = T)),
+        col = my_color(100), 
+        ylab = "Latitude",
+        xlab = "Longitude",
+        xlim = c(-176.5, -156.5),
+        ylim = c(52, 62),
+        zlim = c(0, 1333),
+        main = title,
+        cex.main = 1.2,
+        cex.lab = 1.1,
+        cex.axis = 1.1)
+  maps::map("worldHires",
+            fill = T,
+            col = "wheat4",
+            add = T)
+  image.plot(legend.only = T,
+             col = my_color(100),
+             legend.shrink = 0.2,
+             smallplot = c(.79, .82, .20, .37),
+             legend.cex = 0.8,
+             axis.args = list(cex.axis = 0.8),
+             legend.width = 0.5,
+             legend.mar = 6,
+             zlim = c(0, 1333),
+             legend.args = list("Avg. Predicted \n Occurrence",
+                                side = 2, cex = 1))
+}
 
 #### 2015-2039 ---------------------------------------------------------------------------------------------------------------------
 ##### Eggs
