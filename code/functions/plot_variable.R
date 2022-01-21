84e27b57c641664e7763e7c816f47f15d26cd932
Rebecca Howard <howardrebecca@gmail.com>
2022-01-21 12:24:07 -0800

    Remove extra commenting

--- code/functions/plot_variable.R
+++ code/functions/plot_variable.R
@@ -1,7 +1,7 @@
 plot_variable <- function(gam, covariate, bounds, variable, ylabel, yvalues){
   plot(gam[[2]],
        pages = 0,
-       select = covariate, # 1 = year/PDO/NPGO, 2 = lat/lon, 3 = depth, 4 = julian, 5 = temp
+       select = covariate, 
        shade = T,
        shade.col = "#eb8055b2",
        ylim = bounds,
