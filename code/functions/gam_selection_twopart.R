d7f327663b2937076d8f29974eff310b50d3ed39
Rebecca Howard <howardrebecca@gmail.com>
2022-03-31 10:38:49 -0700

    Add code to use two part delta model

---
+++ code/functions/gam_selection_twopart.R
@@ -0,0 +1,26 @@
+gam_selection <- function(data){
+  gam_base <- gam(catch ~ s(year, bs = 're') + 
+                    s(doy, k = 8) +
+                    s(lon, lat) +
+                    s(roms_temperature, k = 6) +
+                    s(roms_salinity, k = 6),
+                  data = data[data$catch > 0, ])
+  gam_vc_phen <- gam(catch ~ s(year, bs = 're') +
+                       s(doy, k = 8) +
+                       s(lon, lat) +
+                       s(roms_temperature, k = 6) +
+                       s(roms_salinity, k = 6) +
+                       s(doy, by = mean_temp, k = 6),
+                     data = data[data$catch > 0, ])
+  gam_vc_geog <- gam(catch ~ s(year, bs = 're') +
+                       s(doy, k = 8) +
+                       s(lon, lat) +
+                       s(roms_temperature, k = 6) +
+                       s(roms_salinity, k = 6) +
+                       s(lon, lat, by = mean_temp, k = 6),
+                     data = data[data$catch > 0, ])
+  gam_list <- list(gam_base, gam_vc_phen, gam_vc_geog)
+  best_gam <- gam_list[[which.min(sapply(1:length(gam_list),
+                                         function(x) min(gam_list[[x]]$aic)))]]
+  return_list <- list(gam_list, best_gam)
+}
