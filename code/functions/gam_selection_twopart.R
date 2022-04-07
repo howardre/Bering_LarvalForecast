b2b76982d034b1fbe5763187827f0fe8aec74d0f
Rebecca Howard <howardrebecca@gmail.com>
2022-04-07 11:57:01 -0700

    Update two part functions

--- code/functions/gam_selection_twopart.R
+++ code/functions/gam_selection_twopart.R
@@ -1,24 +1,24 @@
 gam_selection <- function(data){
-  gam_base <- gam(catch ~ s(year, bs = 're') + 
+  gam_base <- gam(log(larvalcatchper10m2 + 1) ~ s(year, bs = 're') + 
                     s(doy, k = 8) +
                     s(lon, lat) +
                     s(roms_temperature, k = 6) +
                     s(roms_salinity, k = 6),
-                  data = data[data$catch > 0, ])
-  gam_vc_phen <- gam(catch ~ s(year, bs = 're') +
+                  data = data[data$larvalcatchper10m2 > 0, ])
+  gam_vc_phen <- gam(log(larvalcatchper10m2 + 1) ~ s(year, bs = 're') +
                        s(doy, k = 8) +
                        s(lon, lat) +
                        s(roms_temperature, k = 6) +
                        s(roms_salinity, k = 6) +
                        s(doy, by = mean_temp, k = 6),
-                     data = data[data$catch > 0, ])
-  gam_vc_geog <- gam(catch ~ s(year, bs = 're') +
+                     data = data[data$larvalcatchper10m2 > 0, ])
+  gam_vc_geog <- gam(log(larvalcatchper10m2 + 1) ~ s(year, bs = 're') +
                        s(doy, k = 8) +
                        s(lon, lat) +
                        s(roms_temperature, k = 6) +
                        s(roms_salinity, k = 6) +
                        s(lon, lat, by = mean_temp, k = 6),
-                     data = data[data$catch > 0, ])
+                     data = data[data$larvalcatchper10m2 > 0, ])
   gam_list <- list(gam_base, gam_vc_phen, gam_vc_geog)
   best_gam <- gam_list[[which.min(sapply(1:length(gam_list),
                                          function(x) min(gam_list[[x]]$aic)))]]
