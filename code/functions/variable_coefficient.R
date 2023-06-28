f94aa55440fd7cb808d56eb2b1ba165185781a60
Rebecca Howard <howardrebecca@gmail.com>
2023-06-28 12:07:36 -0700

    Add additional function for if variable is removed

--- code/functions/variable_coefficient.R
+++ code/functions/variable_coefficient.R
@@ -8,3 +8,15 @@ variable_coefficient <- function(gam, data){
   sign_slope_neg <- (1:length(pred_slope))[pred_slope_up < 0]
   return(list(sign_slope_neg, sign_slope_pos, pred_slope))
 }
+
+# Use if a covariate is removed
+variable_coefficient2 <- function(gam, data){
+  preds <- predict(gam[[2]], type = 'terms', se.fit = T)
+  pred_slope <- preds[[1]][, 5] / data$mean_temp
+  pred_slope_se <- 1.95 * preds[[2]][, 5]
+  pred_slope_up <- (preds[[1]][, 5] + pred_slope_se) / data$mean_temp
+  pred_slope_low <- (preds[[1]][, 5] - pred_slope_se) / data$mean_temp
+  sign_slope_pos <- (1:length(pred_slope))[pred_slope_low > 0]
+  sign_slope_neg <- (1:length(pred_slope))[pred_slope_up < 0]
+  return(list(sign_slope_neg, sign_slope_pos, pred_slope))
+}
