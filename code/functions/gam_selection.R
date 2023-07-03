f0467e07116917c24f8ac73f7d8deb1c6185a978
Rebecca Howard <howardrebecca@gmail.com>
2023-07-03 12:55:27 -0700

    Change lon/lat back to smooth

--- code/functions/gam_selection.R
+++ code/functions/gam_selection.R
@@ -1,27 +1,27 @@
 gam_selection <- function(data){
   gam_base <- gam(catch ~ s(year, bs = 're') + 
-                    s(doy, k = 9) +
-                    te(lon, lat) +
-                    s(roms_temperature, k = 9) +
-                    s(roms_salinity, k = 9),
+                    s(doy) +
+                    s(lon, lat) +
+                    s(roms_temperature, k = 5) +
+                    s(roms_salinity, k = 5),
                   data = data,
                   family = tw(link = 'log'),
                   method = 'REML')
   gam_vc_phen <- gam(catch ~ s(year, bs = 're') +
-                       s(doy, k = 9) +
-                       te(lon, lat) +
-                       s(roms_temperature, k = 9) +
-                       s(roms_salinity, k = 9) +
+                       s(doy) +
+                       s(lon, lat) +
+                       s(roms_temperature, k = 5) +
+                       s(roms_salinity, k = 5) +
                        s(doy, by = mean_temp),
                      data = data,
                      family = tw(link = 'log'),
                      method = 'REML')
   gam_vc_geog <- gam(catch ~ s(year, bs = 're') +
-                       s(doy, k = 9) +
-                       te(lon, lat) +
-                       s(roms_temperature, k = 9) +
-                       s(roms_salinity, k = 9) +
-                       te(lon, lat, by = mean_temp),
+                       s(doy) +
+                       s(lon, lat) +
+                       s(roms_temperature, k = 5) +
+                       s(roms_salinity, k = 5) +
+                       s(lon, lat, by = mean_temp),
                      data = data,
                      family = tw(link = 'log'),
                      method = 'REML')
