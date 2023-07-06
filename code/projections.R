d47cdb73f34aa92db9e3b177caccd68b5b53340d
Rebecca Howard <howardrebecca@gmail.com>
2023-07-06 09:28:48 -0700

    Update formulas

--- code/projections.R
+++ code/projections.R
@@ -31,11 +31,11 @@ load_data <- function(file, data, temps){
 # Create GAM formulas
 formula_pheno <- function(data){
   gam(catch ~ s(year, bs = 're') +
-        s(doy, k = 9) +
-        te(lon, lat) +
-        s(roms_temperature, k = 9) +
-        s(roms_salinity, k = 9) +
-        s(doy, by = mean_temp, k = 9), # phenology
+        s(doy, k = 9, bs = "tp", m = 1) +
+        te(lon, lat, bs = "tp", m = 1) +
+        s(roms_temperature, bs = "tp", k = 5, m = 1) +
+        s(roms_salinity, bs = "tp", k = 5, m = 1) +
+        s(doy, by = mean_temp, bs = "tp", m = 1), # phenology
       data = data,
       family = tw(link = "log"),
       method = 'REML')
@@ -43,11 +43,11 @@ formula_pheno <- function(data){
 
 formula_geog <- function(data){
   gam(catch ~ s(year, bs = 're') +
-        s(doy, k = 9) +
-        te(lon, lat, bs = "gp", m = mp, d = 2) +
-        s(roms_temperature, bs = "gp", m = c(-2, r, 0.5)) +
-        s(roms_salinity, bs = "gp", m = c(-2, r, 0.5)) +
-        te(lat, lon, by = mean_temp, bs = "gp", m = mp, d = 2), # geography
+        s(doy, k = 9, bs = "tp", m = 1) +
+        te(lon, lat, bs = "tp", m = 1) +
+        s(roms_temperature, bs = "tp", k = 5, m = 1) +
+        s(roms_salinity, bs = "tp", k = 5, m = 1) +
+        s(lat, lon, by = mean_temp, bs = "tp", m = 1), # geography
       data = data,
       family = tw(link = "log"),
       method = 'REML')
@@ -57,20 +57,26 @@ base_dir <- getwd()
 
 # Gaussian process splines with specified r for Matern function
 # Based on email from Kirsten, code from Jim
-dist <- .1
-x <- seq(0, 25, dist)
-dist <- 0.1
-qlIN <- 0.9
-r <- dist / (1 - qlIN ^ 2)
-
-mp <- list(c(-2, r, 0.5))
+# dist <- 0.1
+# x <- seq(0, 25, dist)
+# qlIN <- 0.9
+# r <- dist / (1 - qlIN ^ 2)
+# 
+# mp <- list(c(-2, r, 0.5))
 
 # Load ROMS temperature means and forecast
 roms_temps <- readRDS(here('data', 'roms_temps.rds'))
 
 ### Pollock Eggs --------------------------------------------------------------------------------------------------------------------------
 pk_egg <- load_data('pk_egg.rds', pk_egg, roms_temps)
-pkegg_formula <- formula_geog(pk_egg)
+pkegg_formula <- gam(catch ~ s(year, bs = 're') +
+                       s(doy, k = 9, bs = "tp", m = 1) +
+                       te(lon, lat, bs = "tp", m = 1) +
+                       s(roms_temperature, bs = "tp", k = 5, m = 1) +
+                       s(lat, lon, by = mean_temp, bs = "tp", m = 1), # geography
+                     data = pk_egg,
+                     family = tw(link = "log"),
+                     method = 'REML') # different for pollock eggs due to SSS removal
 
 #### Forecast and average into 3 time periods ---------------------------------------------------------------------------------------------
 ##### CESM 126 ----------------------------------------------------------------------------------------------------------------------------
