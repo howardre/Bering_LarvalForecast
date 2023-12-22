dd1db571090e85208db8d27308b42e7f55a1a1a8
Rebecca Howard <howardrebecca@gmail.com>
2023-12-21 17:11:43 -0800

    Change dimensions

--- code/functions/map_vc_presentation.R
+++ code/functions/map_vc_presentation.R
@@ -1,6 +1,6 @@
 map_vc_presentation <- function(data, gam, grids){
-  nlat = 80
-  nlon = 120
+  nlat = 40
+  nlon = 60
   latd = seq(min(data$lat), max(data$lat), length.out = nlat)
   lond = seq(min(data$lon), max(data$lon), length.out = nlon)
   myvis_gam(gam,
