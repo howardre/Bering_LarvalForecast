9bb2e7d7617565e5437cae2fa8670dade9534008
Rebecca Howard <howardrebecca@gmail.com>
2024-01-16 14:35:45 -0800

    Change variable plotting

--- code/functions/plot_variable.R
+++ code/functions/plot_variable.R
@@ -3,34 +3,34 @@ plot_variable <- function(gam, covariate, bounds, variable, ylabel, yvalues){
        pages = 0,
        select = covariate, 
        shade = T,
-       shade.col = "#eb8055b2",
+       shade.col = "lightsalmon2",
        ylim = bounds,
        xlab = variable,
        ylab = ylabel,
        yaxt = yvalues,
        seWithMean = T,
        scale = 0,
-       cex.axis = 6.5,
-       cex.lab = 6.5,
+       cex.axis = 6,
+       cex.lab = 6,
        family = "serif",
        lwd = 2.5)
 }
 
-plot_variable_new <- function(gam, covariate, ybounds, xbounds, variable, ylabel, yvalues){
+plot_variable_pk <- function(gam, covariate, bounds, variable, ylabel, yvalues){
   plot(gam[[2]],
        pages = 0,
        select = covariate, 
        shade = T,
-       shade.col = "#eb8055b2",
-       ylim = ybounds,
-       xlim = xbounds,
+       shade.col = "lightsalmon2",
+       yaxp = c(-4, 8, 3),
+       ylim = bounds,
        xlab = variable,
        ylab = ylabel,
        yaxt = yvalues,
        seWithMean = T,
        scale = 0,
-       cex.axis = 6.5,
-       cex.lab = 6.5,
+       cex.axis = 6,
+       cex.lab = 6,
        family = "serif",
        lwd = 2.5)
 }
